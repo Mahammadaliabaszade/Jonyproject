002d66021690e58ed18036554368001e45c589e8
mahammadali.1 <68659317+Mahammadaliabaszade@users.noreply.github.com>
2026-06-06 17:24:44 +0300

    Add files via upload

---
+++ jonyproject-fixed (1)/jonyproject-fixed/js/main.js
@@ -0,0 +1,40 @@
+// function number(n, three, four) {
+//     if (n % 3 == 0)
+//         three();
+//     else if (n % 4 == 0)
+//         four();
+
+
+// }
+
+// function newf1() {
+//     console.log("Eded dorde bolunur");
+// }
+
+// function newf2() {
+//     console.log("Eded uce bolunur");
+
+// }
+
+// function number(n, newf1, newf2)
+function qiymet(bal) {
+
+    if (bal < 51) {
+        console.log("Siz kesilmisiniz")
+    } else if (bal > 50 && bal < 61) {
+        console.log("Siz E almisiniz")
+    }
+
+} else if (bal > 60 && bal < 71) {
+    console.log("Siz D almisiniz")
+} else if (bal > 70 && bal < 81) {
+    console.log("Siz C almisiniz")
+} else if (bal > 80 && bal < 91) {
+    console.log("Siz B almisiniz")
+} else(bal > 90 && bal <= 100) {
+    console.log("Siz A almisiniz")
+}
+
+
+
+}
